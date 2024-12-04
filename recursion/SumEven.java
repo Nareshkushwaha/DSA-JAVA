9e484323ad40be44a1a1e66e01ee5c06cd6d8d4b
naresh <nareshkushwaha8003@gmail.com>
2024-12-04 19:24:30 +0530

    Sum even

---
+++ recursion/SumEven.java
@@ -0,0 +1,20 @@
+package recursion;
+
+public class SumEven {
+    public static int sum(int start, int end) {
+        if (start > end) {
+            return 0; 
+        }
+        if (start % 2 == 0) {
+            return sum(start + 1, end) + start; 
+        }
+        return sum(start + 1, end); 
+    }
+
+    public static void main(String[] args) {
+        int start = 1; 
+        int end = 5; 
+        int result = sum(start, end); 
+        System.out.println("Sum of even numbers between " + start + " and " + end + " is: " + result);
+    }
+}
