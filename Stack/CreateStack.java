814b40f2590c0509820cf289833a257f8519a807
naresh <nareshkushwaha8003@gmail.com>
2024-12-20 21:11:05 +0530

    Create stack

---
+++ Stack/CreateStack.java
@@ -0,0 +1,14 @@
+package Stack;
+
+import java.util.*;
+
+public class CreateStack {
+    public static void main(String[] args) {
+        Stack<Integer> st = new Stack<>();
+        st.push(1);
+        st.push(2);
+        st.push(3);
+        st.push(4);
+        System.out.println(st);
+    }
+}
