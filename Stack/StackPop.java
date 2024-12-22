f73c5eef8356f5dc7eb2b038aa350e551de5f8f3
naresh <nareshkushwaha8003@gmail.com>
2024-12-22 21:46:49 +0530

    Stack pop

---
+++ Stack/StackPop.java
@@ -0,0 +1,21 @@
+package Stack;
+
+import java.util.*;
+
+public class StackPop {
+    public static void main(String[] args) {
+        Stack<Integer> st = new Stack<>();
+        st.push(1);
+        st.push(2);
+        st.push(3);
+        System.out.println(st);
+        st.pop();
+        System.out.println(st);
+        st.pop();
+        System.out.println(st);
+        st.pop();
+        System.out.println(st);
+        st.pop();
+        System.out.println(st);
+    }
+}
