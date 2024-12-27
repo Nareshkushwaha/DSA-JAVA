83a494c3963db8b009f83ff4868b698190bccbd2
naresh <nareshkushwaha8003@gmail.com>
2024-12-27 09:52:36 +0530

    Queue creation

---
+++ Queue/QueueCreation.java
@@ -0,0 +1,11 @@
+package Queue;
+import java.util.*;
+public class QueueCreation{
+    public static void main(String[] args){
+        Queue<Integer> q=new LinkedList<>();
+        q.add(56);
+        q.add(78);
+        q.add(12);
+        System.out.println(q);
+    }
+}
