2fbba39c6db8b15c4fce1fd631cbd444e1e1c271
naresh <nareshkushwaha8003@gmail.com>
2025-01-09 12:00:06 +0530

    Post order traversale

---
+++ Tree/PostOrderTraversale.java
@@ -0,0 +1,48 @@
+package Tree;
+import java.util.Scanner;
+
+public class PostOrderTraversale {
+     static Scanner sc=new Scanner(System.in);
+    static class Node{
+        Node right,left;
+        int data;
+        Node(int data){
+            this.data=data;
+        }
+    }
+    static Node createtree(){
+        Node root=null;
+        System.out.println("enter data");
+        int data=sc.nextInt();
+
+        if(data==-1)
+            return null;
+        root=new Node(data);
+        System.out.println("enter left data "+data );
+        root.left=createtree();
+       
+        System.out.println("enter right data "+data);
+        root.right=createtree();
+       
+        return root;
+    }
+    public static void postOrderTraversal(Node root){
+        if(root==null)
+         return;
+        postOrderTraversal(root.left);
+        postOrderTraversal(root.right);
+        System.out.print(root.data+" ");
+    }
+    public static void main(String[] args) {
+        Node a=createtree();
+        postOrderTraversal(a);
+    }
+}
+
+
+
+
+
+
+
+
